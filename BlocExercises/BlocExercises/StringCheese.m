49aa83d3f720f3550d436cb90255425a8535e261
Michael <michaellosonczy41@gmail.com>
2015-05-08 16:22:54 -0400

    Completed StringCheese.m

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,19 +12,36 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    
+    NSString* answer = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return answer;  
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese"];
+    NSString* answer = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+    return answer;
 }
+
+
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        
+        
+        NSString *modifiableStatement = [NSString stringWithFormat:@"%ld cheese", (long)cheeseCount];
+        return modifiableStatement;
+        
+        
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        
+        NSString *modifiableStatement = [NSString stringWithFormat:@"%ld cheeses", (long)cheeseCount];
+        return modifiableStatement;
+    
     }
     
     /*
